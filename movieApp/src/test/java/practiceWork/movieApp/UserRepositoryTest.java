1098aa43b4e98ee12a6a7a04e7988629756cc397
JoelVaarala <joel.vaarala@myy.haaga-helia.fi>
2020-03-28 18:25:29 +0200

    Added another testing class

---
+++ movieApp/src/test/java/practiceWork/movieApp/UserRepositoryTest.java
@@ -0,0 +1,43 @@
+package practiceWork.movieApp;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertNotNull;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import practiceWork.movieApp.Domain.User;
+import practiceWork.movieApp.Domain.UserRepository;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class UserRepositoryTest {
+	
+	@Autowired
+	private UserRepository userrepository;
+	
+	@Test
+    public void findByIdCheckIfNull() {
+        User user = userrepository.findById(1).get();
+        assertNotNull(user);
+    }
+	
+	@Test
+    public void createNewUser() {
+    	User user = new User("UserT","UserT","User@t.com","USER");
+    	userrepository.save(user);
+    	assertThat(user.getId()).isNotNull();
+    }
+	
+	@Test
+	public void deleteUsers() {
+    	userrepository.deleteAll();
+    	assertThat(userrepository.count()).isEqualTo(0);
+	}
+
+}
